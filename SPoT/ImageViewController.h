0b5ff29f3803b6a93a12b7bdacb2be3df409cba0
Hector Enrique Gomez Morales <hectoregm@gmail.com>
2013-07-04 11:43:50 -0500

    Added ImageViewController from Lecture 9.0

---
+++ SPoT/ImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.h
+//  SPoT
+//
+//  Created by Hector Enrique Gomez Morales on 7/4/13.
+//  Copyright (c) 2013 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@property (nonatomic, strong) NSURL *imageURL;
+
+@end
---
+++ SPoT/ImageViewController.m
@@ -0,0 +1,54 @@
+//
+//  ImageViewController.m
+//  SPoT
+//
+//  Created by Hector Enrique Gomez Morales on 7/4/13.
+//  Copyright (c) 2013 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import "ImageViewController.h"
+
+@interface ImageViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIImageView *imageView;
+@end
+
+@implementation ImageViewController
+
+- (void)setImageURL:(NSURL *)imageURL
+{
+    _imageURL = imageURL;
+    [self resetImage];
+}
+
+- (void)resetImage
+{
+    if (self.scrollView) {
+        self.scrollView.contentSize = CGSizeZero;
+        self.imageView.image = nil;
+        
+        NSData *imageData = [[NSData alloc] initWithContentsOfURL:self.imageURL];
+        UIImage *image = [[UIImage alloc] initWithData:imageData];
+        if (image) {
+            self.scrollView.contentSize = image.size;
+            self.imageView.image = image;
+            self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+        }
+    }
+    
+}
+
+- (UIImageView *)imageView
+{
+    if (_imageView) _imageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+    return _imageView;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.scrollView addSubview:self.imageView];
+    [self resetImage];
+}
+
+@end
