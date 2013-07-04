54c28439e28cca63a94cb0c3f5a7988122d8feb3
Hector Enrique Gomez Morales <hectoregm@gmail.com>
2013-07-04 11:17:13 -0500

    Initial Commit

---
+++ SPoT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPoT
+//
+//  Created by Hector Enrique Gomez Morales on 7/4/13.
+//  Copyright (c) 2013 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SPoT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SPoT
+//
+//  Created by Hector Enrique Gomez Morales on 7/4/13.
+//  Copyright (c) 2013 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
